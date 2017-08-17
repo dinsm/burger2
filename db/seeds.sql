57b5df08a6da3ec99f8c110314c02f82a840c71a
dinsm <denise.mayele@gmail.com>
2017-08-16 23:22:07 -0700

    Down Sequelize

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ("McDeluxe", false);
+INSERT INTO burgers (name, devoured) VALUES ("Chicken Avovado Tomato", false);
+INSERT INTO burgers (name, devoured) VALUES ("Pastrami Cream Green Onions", false);
